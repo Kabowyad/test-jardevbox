d4fc47c65621fa05f4e51a314229347e9b1236f0
yuhnovets <e4CYaaenvGpwRAJ>
2022-03-27 17:46:07 +0500

    Initial commit

---
+++ src/main/java/org/example/WaterCalculator.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class WaterCalculator {
+    public long calculateWaterAmount(int[] landscape) {
+
+            if (landscape == null || landscape.length <= 2) return 0;
+
+            for (int i = 0; i < landscape.length; i++) {
+                if (landscape[i] < 0) {
+                    return 0;
+                }
+            }
+
+            // finding the maximum height and index
+            int max = Integer.MIN_VALUE;
+            int maxIndex = -1;
+            for (int i = 0; i < landscape.length; i++) {
+                if (landscape[i] > max) {
+                    max = landscape[i];
+                    maxIndex = i;
+                }
+            }
+
+            // setting the left edge and moving towards the maximum height
+            // and counting water amount, if current height is smaller then left maximum edge
+            int leftMax = landscape[0];
+            int water = 0;
+            for (int i = 1; i < maxIndex; i++) {
+                if (landscape[i] > leftMax) {
+                    leftMax = landscape[i];
+                } else {
+                    water += leftMax - landscape[i];
+                }
+            }
+
+            // doing the same, but moving from right side
+            int rightMax = landscape[landscape.length - 1];
+            for (int i = landscape.length - 2; i > maxIndex; i--) {
+                if (landscape[i] > rightMax) {
+                    rightMax = landscape[i];
+                } else {
+                    water += rightMax - landscape[i];
+                }
+            }
+
+            return water;
+    }
+}
---
+++ src/test/java/org/example/WaterCalculatorTest.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WaterCalculatorTest extends TestCase {
+
+    public WaterCalculator waterCalculator = new WaterCalculator();
+
+    @Test
+    public void testShouldReturnZeroEmptyInput() {
+        int[] empty = new int[0];
+        long emptyRes = waterCalculator.calculateWaterAmount(empty);
+        Assert.assertEquals(0, emptyRes);
+
+        long nullRes = waterCalculator.calculateWaterAmount(null);
+        Assert.assertEquals(0, nullRes);
+
+        int[] negative = {1,2,-1,1};
+        long negativeRes = waterCalculator.calculateWaterAmount(negative);
+        Assert.assertEquals(0, negativeRes);
+    }
+
+    @Test
+    public void testWorkingScenaries() {
+        int[] first = {2,1,2};
+        long firstRes = waterCalculator.calculateWaterAmount(first);
+        Assert.assertEquals(1, firstRes);
+
+        int[] second = {5,2,3,4,5,4,0,3,1};
+        long secondRes = waterCalculator.calculateWaterAmount(second);
+        Assert.assertEquals(9, secondRes);
+
+        int[] third = {0,0,0};
+        long thirdRes = waterCalculator.calculateWaterAmount(third);
+        Assert.assertEquals(0, thirdRes);
+
+        int[] fourth = {2,5,1,3,1,3,1,5,2};
+        long fourthRes = waterCalculator.calculateWaterAmount(fourth);
+        Assert.assertEquals(16, fourthRes);
+    }
+
+}
